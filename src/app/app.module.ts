c2243c5f1a8dffd17eeec9b93bcb961052b8e476
lazynormz <skupi1234@gmail.com>
2021-11-25 12:45:11 +0100

    Created route for contact.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about-page/about/about.component';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact-page/contact/contact.component';
 import { HomeComponent } from './home/home-page/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent }, 
+  { path: 'contact', component: ContactComponent }
 ]
 
 @NgModule({
