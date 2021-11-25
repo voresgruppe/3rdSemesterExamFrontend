45ed3c930fc1cd14fa554d8692f8b8d3e937d7b0
lazynormz <skupi1234@gmail.com>
2021-11-25 13:00:02 +0100

    Added routing to all the different components in the dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about-page/about/about.component';
 
 import { AppComponent } from './app.component';
+import { BookingComponent } from './bookings/booking-pages/booking/booking.component';
+import { HairstylesComponent } from './bookings/booking-pages/hairstyles/hairstyles.component';
+import { InformationComponent } from './bookings/booking-pages/information/information.component';
 import { ContactComponent } from './contact/contact-page/contact/contact.component';
 import { HomeComponent } from './home/home-page/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'about', component: AboutComponent }, 
-  { path: 'contact', component: ContactComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'hairstyles', component: HairstylesComponent },
+  { path: 'information', component: InformationComponent }
 ]
 
 @NgModule({
