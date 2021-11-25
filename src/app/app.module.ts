b89e9c775c02c715bd879c5d2c9268d0daee7aa6
lazynormz <skupi1234@gmail.com>
2021-11-25 12:38:59 +0100

    Setup routing, though there is a bug needed to be fixed with the nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about-page/about/about.component';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
