5240a278cd90a331961b711115feaf5072a83d5f
lazynormz <skupi1234@gmail.com>
2021-11-25 13:31:45 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about-page/about/about.component';
 
 import { AppComponent } from './app.component';
-import { BookingComponent } from './bookings/booking-pages/booking/booking.component';
-import { HairstylesComponent } from './bookings/booking-pages/hairstyles/hairstyles.component';
-import { InformationComponent } from './bookings/booking-pages/information/information.component';
-import { ContactComponent } from './contact/contact-page/contact/contact.component';
 import { HomeComponent } from './home/home-page/home/home.component';
 
 const routes: Routes = [
