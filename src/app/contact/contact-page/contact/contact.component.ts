69cecece63d4fdf5eef827b6db16e3cc93e23f1a
lazynormz <skupi1234@gmail.com>
2021-12-14 04:47:41 +0100

    As always, converted CSS to SCSS... Why the fuck does this happen, i set it to generate SCSS

--- src/app/contact/contact-page/contact/contact.component.ts
+++ src/app/contact/contact-page/contact/contact.component.ts
@@ -6,7 +6,7 @@ import {observable} from "rxjs";
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
   $employees: Employee[] | undefined;
